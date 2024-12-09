92ace39be74b2241a53d61f2fb635fc74be4de1b
김지우 <ziwookim@triplecomma.co.kr>
2024-12-09 22:17:10 +0900

    feature. 프로젝트 생성

---
+++ src/main/java/com/ziwookim/random_nickname_maker/RandomNicknameMakerApplication.java
@@ -0,0 +1,13 @@
+package com.ziwookim.random_nickname_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNicknameMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomNicknameMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziwookim/random_nickname_maker/RandomNicknameMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziwookim.random_nickname_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNicknameMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
